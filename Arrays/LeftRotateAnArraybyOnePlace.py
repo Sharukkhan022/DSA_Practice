110ba501f2e28217283e0d634b14402959d2c9c8
Sharukkhan022 <sharukkhanpathan226@gmail.com>
2026-06-19 18:06:43 +0530

    efficient version
    Implement left rotation of an array by one place.

--- Arrays/LeftRotateAnArraybyOnePlace.py
+++ Arrays/LeftRotateAnArraybyOnePlace.py
@@ -11,3 +11,14 @@ else :
 
     
 print(array)
+
+array = [1, 2, 3, 4, 5]
+
+if not array:
+    print('Empty array!')
+else:
+    temp = array[0]    # save first element
+    for i in range(1, len(array)):
+        array[i - 1] = array[i]    # shift everyone left
+    array[-1] = temp        # place at end
+    print(array)
